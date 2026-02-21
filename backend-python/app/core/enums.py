2769bcce0dd77b23970d948f572300bf904549f8
Nhu Trinh <nhutrinh6101997@gmail.com>
2026-02-21 12:33:39 +0700

    refactor code

---
+++ backend-python/app/core/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    RECRUITER = "recruiter"
+    CANDIDATE = "candidate"
+
+class Permission(str, Enum):
+    MANAGE_USERS = "manage_users"
+    CREATE_JOB = "create_job"
+    VIEW_JOB = "view_job"
+    UPDATE_JOB = "update_job"
+    DELETE_JOB = "delete_job"
+    APPLY_JOB = "apply_job"
+    
---
+++ backend-python/app/core/security.py
@@ -0,0 +1,75 @@
+import os
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# ================= PASSWORD =================
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+
+# ================= TOKEN =================
+
+def create_access_token(user_id: int, role: str) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
+        "sub": str(user_id),
+        "role": role,
+        "exp": expire
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
+# ================= AUTH =================
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        role = payload.get("role")
+
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+        return {
+            "user_id": user_id,
+            "role": role
+        }
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
+
+
+# ================= ROLE =================
+
+def require_role(roles: list[str]):
+    def role_checker(current_user=Depends(get_current_user)):
+        if current_user["role"] not in roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Permission denied"
+            )
+        return current_user
+    return role_checker
